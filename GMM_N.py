740d9dd8529c67fb249e4733e8e5686c8ad41bdc
Orz <kakinuma4ko@gmail.com>
2017-09-26 23:44:08 +0800

    Update and rename GMM.py to GMM_N.py

--- GMM_N.py
+++ GMM_N.py
@@ -1,3 +1,4 @@
+#_*_coding:utf-8_*_
 import os
 import sys
 if sys.version_info[0] == 2:
@@ -10,50 +11,52 @@ from python_speech_features import mfcc
 from sklearn.mixture import GMM
 from sklearn import preprocessing
 
-
 def get_MFCC(sr, audio):
     return preprocessing.scale(mfcc(audio, sr,  appendEnergy=False))
 
 def train_GMM_N(data_path, model_path, dtype):
-    speaker_list = ['1','2','3','4','5']
+    if os.path.exists(os.path.join(data_path,'.DS_Store')):
+        os.remove(os.path.join(data_path,'.DS_Store')) # for macOS
+    speaker_list = os.listdir(data_path)
+    # exit()
     N = len(speaker_list)
+    # print (N)
+    # exit()
     train_path = [0 for i in range(N)]
-    # train_path = np.zeros([0:N],dtype = np.int16)
-    train_file_list = [([] * 3) for i in range(N)]
+    # train_file_list = [([] * 3) for i in range(N)]
     train_mfcc_features = [0 for i in range(N)]
     speaker_gmm = [0 for i in range(N)]
     
     for i in range(N):
-        # train_path.append(os.path.join(data_path,speaker_list[i]))
-        print i
-        train_path[i] = os.path.join(data_path,speaker_list[i])
-        train_file_list[i] = [os.path.join(train_path[i], f) for f in os.listdir(train_path[i]) if f.endswith('.wav')]
+        # print i
+        # if i < 61:
+            # continue
 
+        # train_path[i] = os.path.join(data_path,speaker_list[i])
+        # train_file_list[i].append(speaker_list[i])
+        # print (train_file_list[i])
         train_mfcc_features[i] = np.asarray((), dtype=dtype)
 
-        for file in train_file_list[i]:
-            f = wave.open(file, 'rb')
-            frame_rate, n_frames = f.getframerate(), f.getnframes()
-            audio = np.fromstring(f.readframes(n_frames), dtype=dtype)
-    #        print audio
-            feature = get_MFCC(frame_rate, audio)
-            if train_mfcc_features[i].size == 0:
-                train_mfcc_features[i] = feature
-            else:
-                train_mfcc_features[i] = np.vstack((mfcc_features, feature))
+        f = wave.open(os.path.join(data_path,speaker_list[i]), 'rb')
+        print os.path.join(data_path,speaker_list[i])
+        frame_rate, n_frames = f.getframerate(), f.getnframes()
+        audio = np.fromstring(f.readframes(n_frames), dtype=dtype)
+    #    print audio
+        feature = get_MFCC(frame_rate, audio)
+        if train_mfcc_features[i].size == 0:
+            train_mfcc_features[i] = feature
+        else:
+            train_mfcc_features[i] = np.vstack((train_mfcc_features[i], feature))
 
         speaker_gmm[i] = GMM(n_components=8, n_iter=200, covariance_type='diag', n_init=3)
         speaker_gmm[i].fit(train_mfcc_features[i])
         pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i]+'.gmm'), 'w'))    
 
-    print train_path
-    print train_file_list
-    print train_mfcc_features
-
 def test_GMM_N(data_path, model_path, dtype):
     gmm_files = [os.path.join(model_path, f) for f in os.listdir(model_path) if f.endswith('.gmm')]
     models = [pickle.load(open(f, 'r')) for f in gmm_files]
     file_list = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.wav')]
+    result = []
     for file in file_list:
         f = wave.open(file, 'rb')
         frame_rate, n_frames = f.getframerate(), f.getnframes()
@@ -63,8 +66,17 @@ def test_GMM_N(data_path, model_path, dtype):
         for i in range(len(models)):
             scores = np.array(models[i].score(feature))
             log_likelihood[i] = scores.sum()
-        print gmm_files[np.argmax(log_likelihood)]
+        print (gmm_files[np.argmax(log_likelihood)])
+        # print log_likelihood
+        result.append(gmm_files[np.argmax(log_likelihood)])
+    acc = 0
+    for i in range(len(file_list)):
+        print (file_list[i],result[i])
+        if file_list[i].split('/')[2].split('.')[0] == result[i].split('/')[2].split('.')[0]:
+            acc += 1
+    print str(acc) + '/67'
+
 
 if __name__ == '__main__':
-    # train_GMM_N('./data', './models', np.int16)
-    test_GMM_N('./test_data', './models', np.int16)
+    train_GMM_N('./train_data', './models', np.int16)
+    # test_GMM_N('./test_data', './models', np.int16)
