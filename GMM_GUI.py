3c91d2f79145f14410aa7fe9d539240b73fc93e2
Orz <kakinuma4ko@gmail.com>
2017-10-21 23:12:38 +0800

    Update GMM_GUI.py

--- GMM_GUI.py
+++ GMM_GUI.py
@@ -1,4 +1,6 @@
-#_*_coding:utf-8_*_
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import os
 import sys
 if sys.version_info[0] == 2:
@@ -13,7 +15,7 @@ from sklearn.mixture import GaussianMixture
 from sklearn import preprocessing
 
 # import tkinter.filedialog as tf
-
+correctrate=float()
 
 import warnings 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -62,10 +64,13 @@ def train_GMM_N(speaker_list, model_path, dtype):
         pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i].split('/')[len(speaker_list[i].split('/'))-1].split('.')[0]+'.gmm'), 'w'))    
 
 def test_GMM_N(file_list, model_path, dtype):
-    gmm_files = [os.path.join(model_path, f) for f in os.listdir(model_path) if f.endswith('.gmm')]
+    gmm_files = []
+    for f in os.listdir(model_path):
+        if f.endswith('.gmm'):
+            gmm_files.append(model_path+'/'+f)
     models = [pickle.load(open(f, 'r')) for f in gmm_files]
     # file_list = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.wav')]
-    print file_list
+    # print 'file_list'+file_list
     result = []
     acc = 0
     for file in file_list:
@@ -80,107 +85,150 @@ def test_GMM_N(file_list, model_path, dtype):
             log_likelihood[i] = scores.sum()
         # print (gmm_files[np.argmax(log_likelihood)])
         # print log_likelihood
-        if file.split('/')[len(file.split('/'))-1].split('.')[0] == gmm_files[np.argmax(log_likelihood)].split('/')[2].split('.')[0]:
+        print file.split('/')[len(file.split('/'))-1].split('.')[0]
+        print gmm_files
+        print gmm_files[np.argmax(log_likelihood)].split('/')[len(gmm_files[np.argmax(log_likelihood)].split('/'))-1].split('.')[0]
+        # if file.split('/')[len(file.split('/'))-1].split('.')[0] == gmm_files[np.argmax(log_likelihood)].split('/')[2].split('.')[0]:
+        if file.split('/')[len(file.split('/'))-1].split('.')[0] ==gmm_files[np.argmax(log_likelihood)].split('/')[len(gmm_files[np.argmax(log_likelihood)].split('/'))-1].split('.')[0]:
+#if 当前文件名 == 测试结果文件名
             acc += 1
-            print acc
+            # print acc
         else:
             print gmm_files[np.argmax(log_likelihood)]
-    # acc = 0
-    # for i in range(len(file_list)):
-    #     # print (file_list[i],result[i])
-    #     if file_list[i].split('/')[2].split('.')[0] == result[i].split('/')[2].split('.')[0]:
-    #         acc += 1
-    print str(acc)
+#     acc = 0
+#     for i in range(len(file_list)):
+#         # print (file_list[i],result[i])
+#         if file_list[i].split('/')[2].split('.')[0] == result[i].split('/')[2].split('.')[0]:
+#             acc += 1
+    print '正确个数'+str(acc)
+    print len(file_list)
+    correctrate=float(acc)/float(len(file_list))*100
+    print correctrate
+    e.set(str(correctrate))
+    # print '正确率'+int(acc)/len(file_list)  #正确个数/总个数
+    if len(file_list) ==1:
+        original_e.set(file.split('/')[len(file.split('/'))-1].split('.')[0])
+        flag_e.set(gmm_files[np.argmax(log_likelihood)].split('/')[len(gmm_files[np.argmax(log_likelihood)].split('/'))-1].split('.')[0])
+    else:
+        original_temp=''
+        flag_temp=''
+        original_e.set(original_temp)
+        flag_e.set(flag_temp)
 
+
+# GUI
+
+import Tkinter as tk
+import tkMessageBox
+import tkFileDialog
+root= tk.Tk()
+root.title("说话人识别")
+root.geometry('395x235')
+path1 = tk.StringVar()
+path2 = tk.StringVar()
+e = tk.StringVar()
+original_e = tk.StringVar()
+flag_e = tk.StringVar()
 train_list = []
 test_list = []
 
+# 读文件
+def selectPath1():
+    # # path_ = tkFileDialog.askdirectory()
+    # # path1.set(path_)
+    # path = tkFileDialog.askopenfilenames()
+    # # print path
+    # # path_train_male.set(path)
+    # train_path = path
+    # global train_list
+    # train_path = train_path[1:-1]
+    # train_list = train_path.split(', ')
+    # print train_list
+    path = tkFileDialog.askopenfilenames()
+    path1.set(path)
+    train_path = path1.get().replace('u\'','').replace('\'','')
+    global train_list
+    train_path = train_path[1:-1]
+    train_list = train_path.split(', ')
+    if len(train_list) == 1:
+        temp = train_list[0]
+        temp = temp[0:-1]
+        train_list[0] = temp
+    tkMessageBox.showinfo(title='提示框', message='训练文件选择完成')
+    # for i in range(0,len(train_list)):
+    #     if i < len(train_list) or i ==len(train_list):
+    #         print train_list[0]
+    #     else:
+    #         print 'end'
+
+def selectPath2():
+    path = tkFileDialog.askopenfilenames()
+    path2.set(path)
+    test_path = path2.get().replace('u\'','').replace('\'','')
+    global test_list
+    test_path = test_path[1:-1]
+    test_list = test_path.split(', ')
+    if len(test_list) == 1:
+        temp = test_list[0]
+        temp = temp[0:-1]
+        test_list[0] = temp
+    tkMessageBox.showinfo(title='提示框', message='测试文件选择完成')
+    # for i in range(0,len(test_list)):
+    #     if i < len(test_list) or i ==len(test_list):
+    #         print test_list[i]
+    #     else:
+    #         print 'end'
+# 训练
+def train():
+    # print 'train'
+    # var1=entry1.get()
+    train_GMM_N(train_list, 'models', np.int16)
+    tkMessageBox.showinfo(title='提示框', message='训练完成')
+
+# 测试
+def test():
+    print('test:')
+    test_GMM_N(test_list, 'models', np.int16)
+    tkMessageBox.showinfo(title='提示框', message='测试完成')
+    # test_GMM_N('a/test_data', 'a/models', np.int16)
+
+
+
+
+# 窗体定义
+tk.Label(root, text=' ').grid(row = 0, column = 0)
+tk.Label(root, text='说话人识别').place(x=165,y=20)
+
+tk.Label(root,text = "  ").grid(row = 1, column = 2)
+
+tk.Label(root,text = "      ").grid(row = 2, column = 0)
+tk.Label(root,text = "训练文件:").grid(row = 2, column = 1)
+entry1=tk.Entry(root, textvariable =path1,borderwidth = 3).grid(row = 2, column = 2)
+tk.Label(root,text = "  ").grid(row = 2, column = 3)
+tk.Button(root, text = "选择", command = selectPath1).grid(row = 2, column = 4)
+tk.Label(root,text = "    ").grid(row = 2, column = 5)
+tk.Button(root, text = "训练", command = train).grid(row = 2, column = 6)
+
+tk.Label(root,text = "  ").grid(row = 3, column = 2)
+
+tk.Label(root,text = "  ").grid(row = 4, column = 0)
+tk.Label(root,text = "测试文件:").grid(row = 4, column = 1)
+entry2=tk.Entry(root, textvariable =path2,borderwidth = 3).grid(row = 4, column = 2)
+tk.Label(root,text = "  ").grid(row = 4, column = 3)
+tk.Button(root, text = "选择", command = selectPath2).grid(row = 4, column = 4)
+tk.Label(root,text = "    ").grid(row = 4, column = 5)
+tk.Button(root, text = "测试", command = test).grid(row = 4, column = 6)
+
+tk.Label(root,text = "  ").grid(row = 5, column = 2)
+
+tk.Label(root,text = "  测试正确率:").place(x=5,y=165)
+tk.Entry(root, textvariable =e,borderwidth = 3,width = 5,).place(x=85,y=165)
+tk.Label(root,text = "  真实结果:").place(x=130,y=152)
+tk.Entry(root, textvariable =original_e,borderwidth = 3,width = 22,).place(x=200,y=152)
+tk.Label(root,text = "  测试结果:").place(x=130,y=182)
+tk.Entry(root, textvariable =flag_e,borderwidth = 3,width = 22,).place(x=200,y=182)
+
 if __name__ == '__main__':
-    import Tkinter as tk
-    import tkFileDialog
-    root= tk.Tk()
-    root.title("说话人识别")
-    root.geometry('500x200')
-    path1 = tk.StringVar()
-    path2 = tk.StringVar()
-
-
-    # 读文件
-    def selectPath1():
-        # # path_ = tkFileDialog.askdirectory()
-        # # path1.set(path_)
-        # path = tkFileDialog.askopenfilenames()
-        # # print path
-        # # path_train_male.set(path)
-        # train_path = path
-        # global train_list
-        # train_path = train_path[1:-1]
-        # train_list = train_path.split(', ')
-        # print train_list
-        path = tkFileDialog.askopenfilenames()
-        path1.set(path)
-        train_path = path1.get().replace('\'','')
-        global train_list
-        train_path = train_path[1:-1]
-        train_list = train_path.split(', ')
-
-        print train_list[0]
-
-    def selectPath2():
-        path = tkFileDialog.askopenfilenames()
-        path2.set(path)
-        test_path = path2.get().replace('\'','')
-        global test_list
-        test_path = test_path[1:-1]
-        test_list = test_path.split(', ')
-    # 训练
-    def train():
-        # print 'train'
-        # var1=entry1.get()
-        train_GMM_N(train_list, 'b/models', np.int16)
-
-    # 测试
-    def test():
-        # print 'test'
-        # print path2.get()
-        test_GMM_N(test_list, 'b/models', np.int16)
-        # test_GMM_N('a/test_data', 'a/models', np.int16)
-
-        # var2 = entry2.get()
-
-    # 窗体定义
-    tk.Label(root, text=' ').grid(row = 0, column = 0)
-    tk.Label(root, text='说话人识别').place(x=165,y=20)
-
-    tk.Label(root,text = "  ").grid(row = 1, column = 2)
-
-    tk.Label(root,text = "      ").grid(row = 2, column = 0)
-    tk.Label(root,text = "训练文件:").grid(row = 2, column = 1)
-    entry1=tk.Entry(root, textvariable =path1,borderwidth = 3).grid(row = 2, column = 2)
-    tk.Label(root,text = "  ").grid(row = 2, column = 3)
-    tk.Button(root, text = "选择", command = selectPath1).grid(row = 2, column = 4)
-    tk.Label(root,text = "    ").grid(row = 2, column = 5)
-    tk.Button(root, text = "训练", command = train).grid(row = 2, column = 6)
-
-    tk.Label(root,text = "  ").grid(row = 3, column = 2)
-
-    tk.Label(root,text = "  ").grid(row = 4, column = 0)
-    tk.Label(root,text = "测试文件:").grid(row = 4, column = 1)
-    entry2=tk.Entry(root, textvariable =path2,borderwidth = 3).grid(row = 4, column = 2)
-    tk.Label(root,text = "  ").grid(row = 4, column = 3)
-    tk.Button(root, text = "选择", command = selectPath2).grid(row = 4, column = 4)
-    tk.Label(root,text = "    ").grid(row = 4, column = 5)
-    tk.Button(root, text = "测试", command = test).grid(row = 4, column = 6)
-
-    tk.Label(root,text = "  ").grid(row = 5, column = 2)
-
-    # tk.Label(root,text = "  测试正确率:").place(x=5,y=152)
-    # tk.Entry(root, textvariable =accc,borderwidth = 3,width = 5,).place(x=85,y=152)
-    # tk.Label(root,text = "  真实结果:").place(x=130,y=152)
-    # tk.Entry(root, textvariable =' ',borderwidth = 3,width = 5,).place(x=200,y=152)
-    # tk.Label(root,text = "  测试结果:").place(x=245,y=152)
-    # tk.Entry(root, textvariable =' ',borderwidth = 3,width = 5,).place(x=315,y=152)
-
     root.mainloop()
 
     # train_GMM_N('a/train_data', 'a/models', np.int16)
