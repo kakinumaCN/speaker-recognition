1dfce63f564c1ab9b8427ae757fbfbff4829512d
Kakinuma4ko <Kakinuma4ko@gmail.com>
2018-08-02 21:48:59 +0800

    commit by alias

--- GMM_N.py
+++ GMM_N.py
@@ -1,6 +1,7 @@
 #_*_coding:utf-8_*_
 import os
 import sys
+import getopt
 if sys.version_info[0] == 2:
     import cPickle as pickle
 else:
@@ -18,10 +19,7 @@ def train_GMM_N(data_path, model_path, dtype):
     if os.path.exists(os.path.join(data_path,'.DS_Store')):
         os.remove(os.path.join(data_path,'.DS_Store')) # for macOS
     speaker_list = os.listdir(data_path)
-    # exit()
     N = len(speaker_list)
-    # print (N)
-    # exit()
     train_path = [0 for i in range(N)]
     # train_file_list = [([] * 3) for i in range(N)]
     train_mfcc_features = [0 for i in range(N)]
@@ -52,6 +50,31 @@ def train_GMM_N(data_path, model_path, dtype):
         speaker_gmm[i].fit(train_mfcc_features[i])
         pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i]+'.gmm'), 'w'))    
 
+def train_GMM(data_path, model_path, dtype):
+    # - train_data
+    #   ├ 1.wav
+    #   ├ 2.wav
+    #   └ ...
+    speaker_list = [f.split('.')[0] for f in os.listdir(data_path) if f.endswith('.png')]
+    N = len(speaker_list)
+    train_mfcc_features = [0 for i in range(N)]
+    speaker_gmm = [0 for i in range(N)]
+    
+    for i in range(N):
+        # f = wave.open(, 'rb')
+        frame_rate, n_frames = f.getframerate(), f.getnframes()
+        audio = np.fromstring(f.readframes(n_frames), dtype=dtype)
+
+        feature = get_MFCC(frame_rate, audio)
+        if train_mfcc_features[i].size == 0:
+            train_mfcc_features[i] = feature
+        else:
+            train_mfcc_features[i] = np.vstack((train_mfcc_features[i], feature))
+
+        speaker_gmm[i] = GMM(n_components=8, n_iter=200, covariance_type='diag', n_init=3)
+        speaker_gmm[i].fit(train_mfcc_features[i])
+        pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i]+'.gmm'), 'w'))   
+
 def test_GMM_N(data_path, model_path, dtype):
     gmm_files = [os.path.join(model_path, f) for f in os.listdir(model_path) if f.endswith('.gmm')]
     models = [pickle.load(open(f, 'r')) for f in gmm_files]
@@ -69,18 +92,19 @@ def test_GMM_N(data_path, model_path, dtype):
         print (gmm_files[np.argmax(log_likelihood)])
         # print log_likelihood
         result.append(gmm_files[np.argmax(log_likelihood)])
-    acc = 0
-    for i in range(len(file_list)):
-        print (file_list[i],result[i])
-        if file_list[i].split('/')[2].split('.')[0] == result[i].split('/')[2].split('.')[0]:
-            acc += 1
-    print str(acc) + '/67'
+    # acc = 0
+    # for i in range(len(file_list)):
+    #     print (file_list[i],result[i])
+    #     if file_list[i].split('/')[2].split('.')[0] == result[i].split('/')[2].split('.')[0]:
+    #         acc += 1
+    # print str(acc) + '/67'
 
 
 if __name__ == '__main__':
-    # train_GMM_N('./train_data', './models', np.int16)
-    # test_GMM_N('./test_data', './models', np.int16)
-    if sys.argv[1] == 'train':
-        train_GMM_N('./train_data', './models', np.int16)
-    elif sys.argv[1] == 'train':
-        test_GMM_N('./test_data', './models', np.int16)
+    
+    opts, args = getopt.getopt(sys.argv[1:],'',['train','test'])
+    for opt, arg in opts:
+        if opt in ('--train'):
+            train_GMM('../train_data', '../models', np.int16)
+        elif opt in ('--test'):
+            test_GMM('../test_data', '../models', np.int16)
