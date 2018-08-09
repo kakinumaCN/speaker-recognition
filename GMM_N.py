efd8800d691355d5cc5e6225fc23c02af72b539e
Kakinuma4ko <Kakinuma4ko@gmail.com>
2018-08-09 17:26:45 +0800

    commit by alias

--- GMM_N.py
+++ GMM_N.py
@@ -34,17 +34,49 @@ def train_GMM(data_path, model_path, dtype):
         audio = np.fromstring(f.readframes(n_frames), dtype=dtype)
 
         feature = get_MFCC(frame_rate, audio)
-        print n_frames, feature.shape
+        print n_frames, feature.shape, feature.size
         train_mfcc_features[i] = feature
 
-        # if train_mfcc_features[i].size == 0:
-        #     train_mfcc_features[i] = feature
-        # else:
-        #     train_mfcc_features[i] = np.vstack((train_mfcc_features[i], feature))
+        speaker_gmm[i] = GaussianMixture(n_components=32, max_iter=200,covariance_type='diag', n_init=3)
+        speaker_gmm[i].fit(train_mfcc_features[i])
+        pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i]+'.gmm'), 'w'))   
+
+def train_GMM_path(data_path, model_path, dtype):
+    # - train_data
+    #   ├ a
+    #     ├ 1.wav
+    #     └ ...
+    #   ├ b
+    #     ├ 1.wav
+    #     └ ...
+    #   └ ...
+    # speaker_list = [f.split('.')[0] for f in os.listdir(data_path) if f.endswith('.wav')] # without'.wav'
+    # speaker_list = [f for f in os.listdir(data_path) if f.endswith('.wav')] # with '.wav'
+    speaker_list = [f for f in os.listdir(data_path) if not f.startswith('.')] # remove .DS_Store
+    N = len(speaker_list)
+    train_mfcc_features = [np.zeros(0) for i in range(N)]
+    speaker_gmm = [0 for i in range(N)]
+
+    for i in range(N):
+        file_list = [f for f in os.listdir(os.path.join(data_path,speaker_list[i])) if f.endswith('.wav')]
+        for file in file_list:
+            f = wave.open(os.path.join(data_path,speaker_list[i],file), 'rb')
+            print speaker_list[i],file
+            frame_rate, n_frames = f.getframerate(), f.getnframes()
+            audio = np.fromstring(f.readframes(n_frames), dtype=dtype)
+
+            feature = get_MFCC(frame_rate, audio)
+            if train_mfcc_features[i].size == 0:
+                train_mfcc_features[i] = feature
+                print 'new feature' ,train_mfcc_features[i].shape 
+            else:
+                train_mfcc_features[i] = np.vstack((train_mfcc_features[i], feature))
+                print 'add feature' ,feature.shape, train_mfcc_features[i].shape
 
         # speaker_gmm[i] = GMM(n_components=8, n_iter=200, covariance_type='diag', n_init=3)
         speaker_gmm[i] = GaussianMixture(n_components=32, max_iter=200,covariance_type='diag', n_init=3)
         speaker_gmm[i].fit(train_mfcc_features[i])
+        print 'fit' + speaker_list[i]
         pickle.dump(speaker_gmm[i], open(os.path.join(model_path, speaker_list[i]+'.gmm'), 'w'))   
 
 def test_GMM(data_path, model_path, dtype):
@@ -73,10 +105,14 @@ def test_GMM(data_path, model_path, dtype):
         print count
 
 if __name__ == '__main__':
-    
+    TRAINPATH = '../train_data'
+    TRAINPATH2 = '/Volumes/Storage/IOS/data/wav/C1_110'
+    TESTPATH = '../test_data'
+    MODELPATH = '../models'
     opts, args = getopt.getopt(sys.argv[1:],'',['train','test'])
     for opt, arg in opts:
         if opt in ('--train'):
-            train_GMM('../train_data', '../models', np.int16)
+            # train_GMM(TRAINPATH, MODELPATH, np.int16)
+            train_GMM_path(TRAINPATH2, MODELPATH, np.int16)
         elif opt in ('--test'):
-            test_GMM('../test_data', '../models', np.int16)
+            test_GMM(TESTPATH, MODELPATH, np.int16)
